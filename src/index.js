85335b0bcb90f8baa600b97264b95b7f61f79276
Mohammad Ahmady Yeganeh <m.yegane49@outlook.com>
2020-02-12 20:48:07 +0330

    Finish the mizuxe theme

--- src/index.js
+++ src/index.js
@@ -3,3 +3,17 @@ import 'bootstrap';
 import jQuery from 'jquery';
 
 jQuery('#year').text(new Date().getFullYear());
+
+jQuery('body').scrollspy({target: '#main-nav'});
+
+jQuery('#main-nav a').on('click', function(event) {
+    if (this.hash !== '') { 
+        event.preventDefault();
+        const hash = this.hash;
+        jQuery('html, body').animate({
+            scrollTop: jQuery(hash).offset().top
+        }, 900, () => {
+            window.location.hash = hash;
+        });
+    }
+});
